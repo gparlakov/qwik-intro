30e81ee3a9ac9c72a0d27aaf0183119d3804a287
Georgi Parlakov <gparlakov@gmail.com>
2024-11-22 09:26:39 +0200

    fat: qwik counter implement

--- counter-qwik/src/routes/index.tsx
+++ counter-qwik/src/routes/index.tsx
@@ -1,15 +1,12 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const count = useSignal(0)
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      count: {count.value}
+      <button onClick$={() => count.value += 1}>+1</button>
     </>
   );
 });
